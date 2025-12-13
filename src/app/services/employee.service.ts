735b588f6da8dae32fe08561750831a8b5c3c2c6
0vitor <w.wutaes@gmail.com>
2025-12-13 13:41:46 -0100

    feat: create employee service

---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,37 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from '../models/employee';
+import { SearchField } from '../models/search-field';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+  private base = `${environment.apiBaseUrl}/employees`;
+  private http = inject(HttpClient);
+
+  list(searchField: SearchField, query?: string): Observable<Employee[]> {
+    if (!query) return this.http.get<Employee[]>(this.base);
+
+    const params = new HttpParams().set(`${searchField}_like`, query);
+    return this.http.get<Employee[]>(this.base, { params });
+  }
+
+  getById(id: number): Observable<Employee> {
+    return this.http.get<Employee>(`${this.base}/${id}`);
+  }
+
+  create(emp: Partial<Employee>): Observable<Employee> {
+    return this.http.post<Employee>(this.base, emp);
+  }
+
+  update(id: number, emp: Partial<Employee>): Observable<Employee> {
+    return this.http.put<Employee>(`${this.base}/${id}`, emp);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.base}/${id}`);
+  }
+}
