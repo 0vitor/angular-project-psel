a3cab3166c2048791c6e08c4bf6e16304133a7c6
0vitor <w.wutaes@gmail.com>
2025-12-13 13:39:47 -0100

    chore: add ngx-toastr

---
+++ src/app/services/notification.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, inject } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable({ providedIn: 'root' })
+export class NotificationService {
+  private toastr = inject(ToastrService);
+
+  success(message: string, title = 'Sucesso') {
+    this.toastr.success(message, title);
+  }
+
+  error(message: string, title = 'Erro') {
+    this.toastr.error(message, title);
+  }
+
+  info(message: string, title = 'Info') {
+    this.toastr.info(message, title);
+  }
+
+  warning(message: string, title = 'Atenção') {
+    this.toastr.warning(message, title);
+  }
+}
