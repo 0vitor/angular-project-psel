66c329e5ddda1bd99a6676039adbf3d24f58e9e1
0vitor <w.wutaes@gmail.com>
2025-12-17 00:01:22 -0300

    feat: translate labels to portuguese

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { PrimeNG } from 'primeng/config';
+
 import { FooterComponent } from '@components/footer/footer';
 import { HeaderComponent } from '@components/header/header';
 
@@ -10,5 +12,41 @@ import { HeaderComponent } from '@components/header/header';
   templateUrl: './app.html',
 })
 export class App {
+  constructor(private primengConfig: PrimeNG) {}
   title: string = 'sergipetec-front';
+
+  ngOnInit() {
+    this.primengConfig.setTranslation({
+      startsWith: 'Começa com',
+      contains: 'Contém',
+      notContains: 'Não contém',
+      endsWith: 'Termina com',
+      equals: 'Igual a',
+      notEquals: 'Diferente de',
+      noFilter: 'Sem filtro',
+
+      lt: 'Menor que',
+      lte: 'Menor ou igual a',
+      gt: 'Maior que',
+      gte: 'Maior ou igual a',
+
+      is: 'É',
+      isNot: 'Não é',
+      before: 'Antes',
+      after: 'Depois',
+      dateIs: 'Data é',
+      dateIsNot: 'Data não é',
+
+      clear: 'Limpar',
+      apply: 'Aplicar',
+      matchAll: 'Corresponder todos',
+      matchAny: 'Corresponder qualquer',
+
+      addRule: 'Adicionar regra',
+      removeRule: 'Remover regra',
+
+      // empty
+      emptyMessage: 'Nenhum resultado encontrado',
+    });
+  }
 }
